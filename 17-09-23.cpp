0489b236821b98e68ae087961f95cbb52e619dd5
Sparsh Saxena <90604939+SPARSH511@users.noreply.github.com>
2023-09-17 16:32:05 +0530

    Create 17-09-23.cpp

---
+++ 17-09-23.cpp
@@ -0,0 +1,17 @@
+//Fibonacci Numbers
+
+vector<long long> printFibb(int n) {
+  //code here
+  if(n == 1) return {1};
+  if(n == 2) return {1,1};
+  long long a=1,b=1,c;
+  vector<long long> ans;
+  ans.push_back(1); ans.push_back(1);
+  for(int i=3;i<=n;i++){
+      c = a+b;
+      ans.push_back(c);
+      a = b;
+      b = c;
+  }
+  return ans;
+}
